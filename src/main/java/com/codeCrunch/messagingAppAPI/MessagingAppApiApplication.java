76b4f8b33a131a5ecf03e9024c5196bc6e8d44b6
alvise1 <acarpin32@gmail.com>
2025-01-06 10:34:14 -0500

    Initial commit

---
+++ src/main/java/com/codeCrunch/messagingAppAPI/MessagingAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.codeCrunch.messagingAppAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingAppApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeCrunch/messagingAppAPI/MessagingAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeCrunch.messagingAppAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
